34613ba6b47b32173065179cfc8c5d07631cc1ec
JohnnyOfSnow <good258johnny@yahoo.com.tw>
2019-03-27 12:47:47 +0900

    Handle the JQuery not be used in html file

--- ch12_loginSystem/login.js
+++ ch12_loginSystem/login.js
@@ -1,64 +1,19 @@
-var express = require ('express');
-var app = express ();
-var session = require ('express-session');
-var FileStroe = require ('session-file-store');
-
-var identityKey = 'skey';
-
-app.use (session({
-	name: identityKey,
-	secret: 'mashroom',
-	store: new FileStroe (),
-	saveUninitialized: false,
-	resave: false,
-	cookie: { maxAge: 600 * 1000} // ten minutes expired.
-}));
-
-var users = require ('./users').items;
-
-var findUser = function (name, password){
-  return users.find (function (item){
-  	return item.name === name && item.password === password;
-  })
-}
-
-//Login interface
-app.post ('/login', function (req, res, next){
-  var sess = req.session;
-  var user = findUser (req.body.name, req.body.password);
-
-  if (user){
-  	req.session.regenerate (function (err){
-  	  if (err){
-  	    return res.json ({ ret_code: 2, ret_msg: '登入失敗'});
-  	  }
-  		req.session.loginUser = user.name;
-  		res.json ({ ret_code: 0, ret_msg: '登入成功'});
-  	});
-  	  res.json ({ ret_code: 1, ret_msg: '帳號或密碼認證錯誤'});
-  }
-});
-
-//Logout interface
-app.post ('/logout', function (req, res, next){
-  req.session.distroy (function (err){
-  	if (err){
-  	  res.json ({ ret_code: 2, ret_msg: '退出登入失敗'});
-  	  return;
-  	}
-  	res.clearCookie(identityKey);
-  	res.redirect('/');
-  });
-});
-
-app.get ('/', function (req, res, next){
-  var sess = req.session;
-  var loginUser = sess.loginUser;
-  var isLogined = !!loginUser;
-
-  res.render ('index', {
-  	isLogined: isLogined,
-  	name: loginUser || ''
-  });
-});
+$('document').ready(function(){
+	$('#submit_button').click(function(){
+		alert('Button click');
+		//var userId = $("#userId").val();
+		//var userMessage = $("#userMessage").val();
+		var reqdata = $('#loginForm').find ($('#userMessage')).val ();
+		//alert("Name: " + userName + " ,Password: " + userPassword);
+		//Give data to the backend.
+		$.ajax({
+			url: '/restful/todo',
+			type: 'post',
+			data: reqdata
+		}).done (function (result){
+			alert('ok!');
+			//$('#itemset').html (result);
+		});
+	})
+})
 
